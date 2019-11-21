7b49ae27f3a5b968bbe09e1c7dcfc8b9eb91b2df
GameSpriter <GameSpriter589@gmail.com>
2019-11-21 02:42:24 -0500

    Added Item Class. Added Address Class.

---
+++ MomsAndPopsPizzaria/Address.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace MomsAndPopsPizzaria
+{
+    public class Address
+    {
+        public string Street, Appartment, City, State, Zipcode, Country;
+
+        public Address()
+        {
+
+        }
+
+        public override string ToString()
+        {
+            return base.ToString();
+        }
+    }
+}
---
+++ MomsAndPopsPizzaria/Item.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace MomsAndPopsPizzaria
+{
+    public class Item
+    {
+        public string Name;
+        public int Count;
+        public float Price;
+    }
+}
