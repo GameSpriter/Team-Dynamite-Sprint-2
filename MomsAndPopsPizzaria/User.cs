54ba8211b591c518c62ca2733cdc474ab360a8b0
GameSpriter <GameSpriter589@gmail.com>
2019-11-21 02:48:03 -0500

    Added User class.

---
+++ MomsAndPopsPizzaria/User.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace MomsAndPopsPizzaria
+{
+    public class User
+    {
+        public string FirstName, LastName, EmailAddress, PhoneNumber;
+        public DateTime birthdate;
+        public Address address;
+
+        private string Password;
+
+        public User()
+        {
+
+        }
+
+        public void VerifyPassword()
+        {
+
+        }
+    }
+}
