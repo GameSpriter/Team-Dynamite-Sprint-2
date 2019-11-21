e9c75b56b1a1f27ae594645904525c37d21ba746
GameSpriter <GameSpriter589@gmail.com>
2019-11-21 02:56:58 -0500

    Added Pizza Class

---
+++ MomsAndPopsPizzaria/Pizza.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace MomsAndPopsPizzaria
+{
+    public class Pizza
+    {
+        public enum Crust { };
+        public Crust SelectedCrust;
+        public enum Size { };
+        public Size SelectedSize;
+        public List<Item> ToppingsList;
+        public float price;
+
+        public Pizza()
+        {
+
+        }
+
+        public void addTopping(Item topping)
+        {
+
+        }
+
+        public void addTopping(string topping)
+        {
+
+        }
+
+        private void updatePrice()
+        {
+
+        }
+    }
+}
