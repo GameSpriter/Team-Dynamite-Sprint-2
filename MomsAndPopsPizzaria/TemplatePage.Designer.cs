862f86d0ac489b3973f7333571948971d70b5099
GameSpriter <GameSpriter589@gmail.com>
2019-11-22 18:36:32 -0500

    Modified TemplatePage and built again

--- MomsAndPopsPizzaria/TemplatePage.Designer.cs
+++ MomsAndPopsPizzaria/TemplatePage.Designer.cs
@@ -30,12 +30,14 @@
         {
             this.topBarTemplate1 = new MomsAndPopsPizzaria.TemplateTopBar();
             this.bottomBarTemplate1 = new MomsAndPopsPizzaria.TemplateBottomBar();
+            this.PageTitle = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // topBarTemplate1
             // 
             this.topBarTemplate1.BackColor = System.Drawing.Color.Gainsboro;
             this.topBarTemplate1.Location = new System.Drawing.Point(0, 0);
+            this.topBarTemplate1.Margin = new System.Windows.Forms.Padding(0);
             this.topBarTemplate1.Name = "topBarTemplate1";
             this.topBarTemplate1.Size = new System.Drawing.Size(1280, 160);
             this.topBarTemplate1.TabIndex = 0;
@@ -44,20 +46,33 @@
             // 
             this.bottomBarTemplate1.BackColor = System.Drawing.Color.Gainsboro;
             this.bottomBarTemplate1.Location = new System.Drawing.Point(0, 680);
+            this.bottomBarTemplate1.Margin = new System.Windows.Forms.Padding(0);
             this.bottomBarTemplate1.Name = "bottomBarTemplate1";
             this.bottomBarTemplate1.Size = new System.Drawing.Size(1280, 40);
             this.bottomBarTemplate1.TabIndex = 1;
             // 
-            // PageTemplate
+            // PageTitle
+            // 
+            this.PageTitle.AutoSize = true;
+            this.PageTitle.Font = new System.Drawing.Font("Microsoft Sans Serif", 22F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.PageTitle.Location = new System.Drawing.Point(4, 164);
+            this.PageTitle.Name = "PageTitle";
+            this.PageTitle.Size = new System.Drawing.Size(140, 36);
+            this.PageTitle.TabIndex = 2;
+            this.PageTitle.Text = "PageTitle";
+            // 
+            // TemplatePage
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Controls.Add(this.PageTitle);
             this.Controls.Add(this.bottomBarTemplate1);
             this.Controls.Add(this.topBarTemplate1);
             this.Margin = new System.Windows.Forms.Padding(0);
-            this.Name = "PageTemplate";
+            this.Name = "TemplatePage";
             this.Size = new System.Drawing.Size(1280, 720);
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
@@ -65,5 +80,6 @@
 
         private TemplateTopBar topBarTemplate1;
         private TemplateBottomBar bottomBarTemplate1;
+        private System.Windows.Forms.Label PageTitle;
     }
 }
