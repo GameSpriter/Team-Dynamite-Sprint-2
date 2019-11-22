b1a374f0b33631175d8a93f5160952bf27c01e1c
GameSpriter <GameSpriter589@gmail.com>
2019-11-22 14:50:32 -0500

    Added NameTemplate.cs for collecting Names(used at least twice)

---
+++ MomsAndPopsPizzaria/NameTemplate.Designer.cs
@@ -0,0 +1,91 @@
+﻿namespace MomsAndPopsPizzaria
+{
+    partial class NameTemplate
+    {
+        /// <summary> 
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary> 
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Component Designer generated code
+
+        /// <summary> 
+        /// Required method for Designer support - do not modify 
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.FirstNameText = new System.Windows.Forms.Label();
+            this.LastNameText = new System.Windows.Forms.Label();
+            this.FirstNameTextBox = new System.Windows.Forms.TextBox();
+            this.LastNameTextBox = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // FirstNameText
+            // 
+            this.FirstNameText.AutoSize = true;
+            this.FirstNameText.Location = new System.Drawing.Point(34, 21);
+            this.FirstNameText.Name = "FirstNameText";
+            this.FirstNameText.Size = new System.Drawing.Size(35, 13);
+            this.FirstNameText.TabIndex = 0;
+            this.FirstNameText.Text = "label1";
+            // 
+            // LastNameText
+            // 
+            this.LastNameText.AutoSize = true;
+            this.LastNameText.Location = new System.Drawing.Point(128, 10);
+            this.LastNameText.Name = "LastNameText";
+            this.LastNameText.Size = new System.Drawing.Size(35, 13);
+            this.LastNameText.TabIndex = 1;
+            this.LastNameText.Text = "label1";
+            // 
+            // FirstNameTextBox
+            // 
+            this.FirstNameTextBox.Location = new System.Drawing.Point(3, 49);
+            this.FirstNameTextBox.Name = "FirstNameTextBox";
+            this.FirstNameTextBox.Size = new System.Drawing.Size(141, 20);
+            this.FirstNameTextBox.TabIndex = 2;
+            // 
+            // LastNameTextBox
+            // 
+            this.LastNameTextBox.Location = new System.Drawing.Point(167, 39);
+            this.LastNameTextBox.Name = "LastNameTextBox";
+            this.LastNameTextBox.Size = new System.Drawing.Size(124, 20);
+            this.LastNameTextBox.TabIndex = 3;
+            // 
+            // NameTemplate
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Controls.Add(this.LastNameTextBox);
+            this.Controls.Add(this.FirstNameTextBox);
+            this.Controls.Add(this.LastNameText);
+            this.Controls.Add(this.FirstNameText);
+            this.Name = "NameTemplate";
+            this.Size = new System.Drawing.Size(312, 81);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label FirstNameText;
+        private System.Windows.Forms.Label LastNameText;
+        private System.Windows.Forms.TextBox FirstNameTextBox;
+        private System.Windows.Forms.TextBox LastNameTextBox;
+    }
+}
