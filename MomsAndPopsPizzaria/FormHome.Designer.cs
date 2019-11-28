c610923c293178bad0be907e22db7e193e5c37c4
rnafiu <rnafiu@gmail.com>
2019-11-28 15:34:19 -0500

    Initial commit. Added functionality to go from Home to Login page.

--- MomsAndPopsPizzaria/FormHome.Designer.cs
+++ MomsAndPopsPizzaria/FormHome.Designer.cs
@@ -41,68 +41,76 @@
             // 
             // LoginButton
             // 
-            this.LoginButton.Location = new System.Drawing.Point(121, 227);
+            this.LoginButton.Location = new System.Drawing.Point(161, 279);
+            this.LoginButton.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.LoginButton.Name = "LoginButton";
-            this.LoginButton.Size = new System.Drawing.Size(75, 23);
+            this.LoginButton.Size = new System.Drawing.Size(100, 28);
             this.LoginButton.TabIndex = 0;
             this.LoginButton.Text = "Login";
             this.LoginButton.UseVisualStyleBackColor = true;
+            this.LoginButton.Click += new System.EventHandler(this.LoginButton_Click);
             // 
             // MenuButton
             // 
-            this.MenuButton.Location = new System.Drawing.Point(460, 199);
+            this.MenuButton.Location = new System.Drawing.Point(613, 245);
+            this.MenuButton.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.MenuButton.Name = "MenuButton";
-            this.MenuButton.Size = new System.Drawing.Size(75, 23);
+            this.MenuButton.Size = new System.Drawing.Size(100, 28);
             this.MenuButton.TabIndex = 1;
             this.MenuButton.Text = "Menu";
             this.MenuButton.UseVisualStyleBackColor = true;
+            this.MenuButton.Click += new System.EventHandler(this.MenuButton_Click);
             // 
             // pageTemplate1
             // 
             this.pageTemplate1.Location = new System.Drawing.Point(0, 0);
             this.pageTemplate1.Margin = new System.Windows.Forms.Padding(0);
             this.pageTemplate1.Name = "pageTemplate1";
-            this.pageTemplate1.Size = new System.Drawing.Size(1280, 720);
+            this.pageTemplate1.Size = new System.Drawing.Size(1707, 886);
             this.pageTemplate1.TabIndex = 2;
             // 
             // pictureBox1
             // 
             this.pictureBox1.BackColor = System.Drawing.Color.Gainsboro;
-            this.pictureBox1.Location = new System.Drawing.Point(98, 305);
+            this.pictureBox1.Location = new System.Drawing.Point(131, 375);
+            this.pictureBox1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Size = new System.Drawing.Size(266, 231);
+            this.pictureBox1.Size = new System.Drawing.Size(355, 284);
             this.pictureBox1.TabIndex = 3;
             this.pictureBox1.TabStop = false;
             // 
             // pictureBox2
             // 
             this.pictureBox2.BackColor = System.Drawing.Color.Gainsboro;
-            this.pictureBox2.Location = new System.Drawing.Point(405, 304);
+            this.pictureBox2.Location = new System.Drawing.Point(540, 374);
+            this.pictureBox2.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.pictureBox2.Name = "pictureBox2";
-            this.pictureBox2.Size = new System.Drawing.Size(266, 231);
+            this.pictureBox2.Size = new System.Drawing.Size(355, 284);
             this.pictureBox2.TabIndex = 4;
             this.pictureBox2.TabStop = false;
             // 
             // pictureBox3
             // 
             this.pictureBox3.BackColor = System.Drawing.Color.Gainsboro;
-            this.pictureBox3.Location = new System.Drawing.Point(724, 304);
+            this.pictureBox3.Location = new System.Drawing.Point(965, 374);
+            this.pictureBox3.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.pictureBox3.Name = "pictureBox3";
-            this.pictureBox3.Size = new System.Drawing.Size(266, 230);
+            this.pictureBox3.Size = new System.Drawing.Size(355, 283);
             this.pictureBox3.TabIndex = 5;
             this.pictureBox3.TabStop = false;
             // 
             // FormHome
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1280, 720);
+            this.ClientSize = new System.Drawing.Size(1707, 886);
             this.Controls.Add(this.pictureBox3);
             this.Controls.Add(this.pictureBox2);
             this.Controls.Add(this.pictureBox1);
             this.Controls.Add(this.MenuButton);
             this.Controls.Add(this.LoginButton);
             this.Controls.Add(this.pageTemplate1);
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.Name = "FormHome";
             this.Text = "MainPage";
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
--- MomsAndPopsPizzaria/FormHome.cs
+++ MomsAndPopsPizzaria/FormHome.cs
@@ -12,9 +12,48 @@ namespace MomsAndPopsPizzaria
 {
     public partial class FormHome : Form
     {
+        FormLogin loginForm = new FormLogin();
         public FormHome()
         {
             InitializeComponent();
+        }
+
+        /// <summary>
+        /// Takes the user to the login form
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void LoginButton_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                loginForm.Show();
+                
+
+            }
+            
+            catch (ObjectDisposedException ex)
+            {
+                throw ex;
+            }
+            finally
+            {
+                this.Hide();
+            }
+
+        }
+
+        private void MenuButton_Click(object sender, EventArgs e)
+        {
+            try
+            {
+
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
         }
     }
 }
