d6940e4f80f0971de394b158874d6f3196cc4e6b
Kai Stoessel <kstoesse@students.kennesaw.edu>
2019-11-29 09:34:51 -0500

    This is my first look at the Pizza class.

--- MomsAndPopsPizzaria/Pizza.cs
+++ MomsAndPopsPizzaria/Pizza.cs
@@ -7,31 +7,76 @@ namespace MomsAndPopsPizzaria
 {
     public class Pizza
     {
-        public enum Crusts { };
+        public enum Crusts 
+        {
+            regular = 0,
+            thin = 1,
+            pan = 2,
+        };
         public Crusts SelectedCrust;
-        public enum Sizes { };
+        public enum Sizes 
+        {
+            small = 0,
+            medium = 1,
+            large = 2,
+            extraLarge = 3,
+        };
         public Sizes SelectedSize;
-        public enum Toppings { };
+        public enum Toppings 
+        {
+            cheese = 0,
+            ham = 1,
+            tomatoes = 2,
+            pepperoni = 3,
+            greenPeppers = 4,
+            mushrooms = 5,
+            sausage = 6,
+            onions = 7,
+            pineapple = 8,
+        };
         public List<Item> SelectedToppings;
         public float price;
 
-        public Pizza()
+        public Pizza(Crusts c, Size s, List<Item> st)
         {
-
+            SelectedCrust = c;
+            SelectedSize = s;
+            SelectedToppings = st;
         }
 
         public void addTopping(Item topping)
         {
-
+            SelectedToppings.Add(topping);
         }
 
         public void addTopping(string topping)
         {
-
+            //What is the purpose of this method?
         }
 
         private void updatePrice()
         {
+            if(SelectedSize == 0) 
+            {
+                price  += 4.00;
+            }
+            else if(SelectedSize == 1)
+            {
+                price += 6.00;
+            }
+            else if(SelectedSize == 2)
+            {
+                price += 8.00;
+            } 
+            else 
+            {
+                price += 10.00;
+            }
+
+            if(SelectedToppings.Count > 1)
+            {
+                price += 1.00;
+            }
 
         }
     }
