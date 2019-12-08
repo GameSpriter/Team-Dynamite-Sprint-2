6a0dcfffae435686d320e57316e52536986dd753
Kai Stoessel <kstoesse@students.kennesaw.edu>
2019-12-08 13:13:30 -0500

    Adding changes to branch

--- MomsAndPopsPizzaria/Item.cs
+++ MomsAndPopsPizzaria/Item.cs
@@ -10,5 +10,13 @@ namespace MomsAndPopsPizzaria
         public string Name;
         public int Count;
         public float Price;
+
+        public Item(string n, int c, float p)
+        {
+            Name = n;
+            Count = c;
+            Price = p;
+        }
     }
+
 }
