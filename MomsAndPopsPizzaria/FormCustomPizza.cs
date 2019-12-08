3894f4c8a85fcbcadf9de06144bbd497e3fa9ae1
GameSpriter <GameSpriter589@gmail.com>
2019-12-07 19:48:28 -0500

    Added RadioButtonFunctionality for sizes.

--- MomsAndPopsPizzaria/FormCustomPizza.cs
+++ MomsAndPopsPizzaria/FormCustomPizza.cs
@@ -13,10 +13,7 @@ namespace MomsAndPopsPizzaria
     public partial class FormCustomPizza : Form
     {
         private Pizza pizza;
-        private enum Toppings
-        {
-            Cheese, Pepperoni, Onion
-        }
+        private float toppingPrice;
         FormController Parent;
 
         public FormCustomPizza(FormController Parent)
@@ -36,7 +33,8 @@ namespace MomsAndPopsPizzaria
             int i = 0;
             foreach(TemplatePizzaTopping template in templatePizzaToppingsCollection)
             {
-                //template.Topping = new Item(Enum.GetName(typeof(Toppings), i);
+
+                template.Topping = new Item(Enum.GetName(typeof(Pizza.Toppings), i), 1, toppingPrice);
                 i++;
             }
         }
@@ -48,27 +46,35 @@ namespace MomsAndPopsPizzaria
 
         private void CancelButton_Click(object sender, EventArgs e)
         {
+            pizza = null;
 
         }
 
         private void SmallSizeRadioButton_CheckedChanged(object sender, EventArgs e)
         {
-
+            toppingPrice = 0.50f;
+            pizza.SelectedSize = Pizza.Sizes.small;
         }
 
         private void MeduimSizeRadioButton_CheckedChanged(object sender, EventArgs e)
         {
 
+            toppingPrice = 0.75f;
+            pizza.SelectedSize = Pizza.Sizes.medium;
         }
 
         private void LargeSizeRadioButton_CheckedChanged(object sender, EventArgs e)
         {
 
+            toppingPrice = 1.00f;
+            pizza.SelectedSize = Pizza.Sizes.large;
         }
 
         private void ExtraLargeSizeRadioButton_CheckedChanged(object sender, EventArgs e)
         {
 
+            toppingPrice = 1.25f;
+            pizza.SelectedSize = Pizza.Sizes.extraLarge;
         }
 
         private void AddToCartButton_Click(object sender, EventArgs e)
